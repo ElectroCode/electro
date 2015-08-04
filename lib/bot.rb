b38f0e6499b6419cacb196afc7678baaa58e8af4
Ken Spencer <iota@electrocode.net>
2015-08-04 07:35:05 -0400

    Fix regex?

--- lib/bot.rb
+++ lib/bot.rb
@@ -6,7 +6,7 @@
 
 class WelcomePlugin
 	include Cinch::Plugin
-	match /(*)/, react_to: :PRIVMSG, method: :doWelcome 
+	match /(.*)/, react_to: :PRIVMSG, method: :doWelcome 
 	
 	def doWelcome(m,bleh)
 		if m.channel == "#debug"
